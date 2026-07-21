4f2313eecd7a93d1ed81437b161424b2bd1bb33b
Logan Sulpizio <logan.sulpizio@widetech.com>
2026-07-21 17:16:42 +0200

    empty commit to force a github action re-run

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BLOCK_BUFFER = 7                        # Gap between the latest block available
 TIME_TO_WAIT_BEFORE_RETRY = 2           # time to wait before retry when RPC is not available
 MAX_RETRIES_PER_BLOCK_RANGE = 7         # Number of time the request will be retried when it has failed before changing the RPC
 COUNT_PERIODIC_BACKFILL_THEGRAPH = 960  # Number of iteration before backfilling the blocks into the DB the blocks of the last few hours (with TheGraph)
-EXPORT_EVENTS_TO_EVENT_QUEUE = False    # Export events to event_queue table (True or False)                      
+EXPORT_EVENTS_TO_EVENT_QUEUE = False    # Export events to event_queue table (True or False).                     
